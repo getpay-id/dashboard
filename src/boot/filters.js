4598abbb00331851e4a0002704bb960baedb9f64
aprilahijriyan <hijriyan23@gmail.com>
2022-07-21 07:18:30 +0700

    🐞 fix(core): if date time does not provide timezone, add UTC timezone as default.

--- src/boot/filters.js
+++ src/boot/filters.js
@@ -7,6 +7,9 @@ export default boot(async ({ app }) => {
   // You can create filters here, I recommend using the '$' prefix before the function name.
   app.config.globalProperties.$formatDate = function (dateString) {
     if (dateString) {
+      if (!dateString.includes("+")) {
+        dateString = dateString + "+00:00";
+      }
       const date = new Date(dateString);
       return moment(date).tz("Asia/Jakarta").format("DD/MM/YYYY, h:mm a");
     }
